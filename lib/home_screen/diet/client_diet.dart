5f29f1012812477960ad613008f782b7f4a414bd
Chiren <shah.chiren25@gmail.com>
2020-11-18 00:12:32 +0530

    diet animation added

--- lib/home_screen/diet/client_diet.dart
+++ lib/home_screen/diet/client_diet.dart
@@ -9,139 +9,59 @@ import 'package:firebase_auth/firebase_auth.dart';
 FirebaseAuth auth = FirebaseAuth.instance;
 String current = auth.currentUser.phoneNumber;
 
-class Diet extends StatelessWidget {
-  Diet();
+class Diet extends StatefulWidget {
+  DietState createState() => new DietState();
+}
+
+class DietState extends State<Diet> {
+  @override
+  void initState() {
+    _loadItems();
+    super.initState();
+  }
+
+  Map<String, dynamic> data;
+  final GlobalKey<AnimatedListState> _listkey = GlobalKey<AnimatedListState>();
+  final Tween<Offset> offset = Tween(begin: Offset(1, 0), end: Offset(0, 0));
+  List<dynamic> _items = [];
+  int counter = 0;
+  List<dynamic> test = [];
+
+  Future<void> _loadItems() async {
+    FirebaseFirestore.instance
+        .collection('Users')
+        .doc('+918976305456')
+        .get()
+        .then((DocumentSnapshot documentSnapshot) => {
+              documentSnapshot.data()['diet'].forEach((key, value) {
+                print(value);
+                test.add(value);
+              }),
+              please(test),
+            });
+  }
+
+  please(text) async {
+    for (var item in text) {
+      // 1) Wait for one second
+      await Future.delayed(Duration(milliseconds: 200));
+      // 2) Adding data to actual variable that holds the item.
+      _items.add(item);
+      // 3) Telling animated list to start animation
+      _listkey.currentState.insertItem(_items.length - 1);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    CollectionReference appointment =
-        FirebaseFirestore.instance.collection('Users');
-
-    return FutureBuilder<DocumentSnapshot>(
-      future: appointment.doc('+918976305456').get(),
-      builder:
-          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-        if (snapshot.hasError) {
-          return Text("Something went wrong");
-        }
-
-        if (snapshot.connectionState == ConnectionState.done) {
-          Map<String, dynamic> data = snapshot.data.data();
-          print(data);
-          return Scaffold(
-            body: Padding(
-                padding: const EdgeInsets.all(13.0),
-                child: Container(
-                  child: ListView(
-                    children: [
-                      for (var key in data['diet'].keys.toList()..sort())
-                        if (key.contains('AM'))
-                          Column(children: [
-                            Card(
-                                color: const Color(0xfff6fef6),
-                                child: InkWell(
-                                    splashColor: Colors.green.withAlpha(30),
-                                    onTap: () {
-                                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder: (context) =>
-                                                Recipe(name: data['diet'][key]),
-                                          ));
-                                    },
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(11.0),
-                                      child: Container(
-                                        height: 50,
-                                        child: Row(
-                                          children: [
-                                            Text(
-                                              key,
-                                              style: TextStyle(
-                                                  fontSize: 15,
-                                                  fontWeight: FontWeight.bold,
-                                                  // color: Colors.green[900]),
-                                                  color: Colors.blueGrey[800]),
-                                            ),
-                                            VerticalDivider(
-                                              width: 35,
-                                              thickness: 1.1,
-                                            ),
-                                            Expanded(
-                                              child: Text(
-                                                data['diet'][key],
-                                                style: TextStyle(
-                                                    fontSize: 18,
-                                                    fontWeight: FontWeight.w600,
-                                                    // color: Colors.blueGrey[800]),
-                                                    color: Colors.green[900]),
-                                              ),
-                                            ),
-                                            Icon(Icons.arrow_right,
-                                                color: Colors.green[900])
-                                          ],
-                                        ),
-                                      ),
-                                    )))
-                          ]),
-                      for (var key in data['diet'].keys.toList()..sort())
-                        if (key.contains('PM'))
-                          Column(children: [
-                            Card(
-                                color: const Color(0xfff6fef6),
-                                child: InkWell(
-                                    splashColor: Colors.green.withAlpha(30),
-                                    onTap: () {
-                                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder: (context) =>
-                                                Recipe(name: data['diet'][key]),
-                                          ));
-                                    },
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(11.0),
-                                      child: Container(
-                                        height: 50,
-                                        child: Row(
-                                          children: [
-                                            Text(
-                                              key,
-                                              style: TextStyle(
-                                                  fontSize: 15,
-                                                  fontWeight: FontWeight.bold,
-                                                  // color: Colors.green[900]),
-                                                  color: Colors.blueGrey[800]),
-                                            ),
-                                            VerticalDivider(
-                                              width: 35,
-                                              thickness: 1.1,
-                                            ),
-                                            Expanded(
-                                              child: Text(
-                                                data['diet'][key],
-                                                style: TextStyle(
-                                                    fontSize: 18,
-                                                    fontWeight: FontWeight.w600,
-                                                    // color: Colors.blueGrey[800]),
-                                                    color: Colors.green[900]),
-                                              ),
-                                            ),
-                                            Icon(Icons.arrow_right,
-                                                color: Colors.green[900])
-                                          ],
-                                        ),
-                                      ),
-                                    )))
-                          ]),
-                    ],
-                  ),
-                )),
+    return AnimatedList(
+        key: _listkey,
+        initialItemCount: _items.length,
+        itemBuilder: (context, index, animate) {
+          return SlideTransition(
+            child: Text(_items[index]),
+            position: animate.drive(offset),
           );
-        }
-
-        return Text('loading');
-      },
-    );
+        });
   }
 }
