36eb74c39c9a8d177f3c368f5057c49c41bf0505
Chiren <shah.chiren25@gmail.com>
2020-11-26 15:22:31 +0530

    added previous appointment

---
+++ lib/home_screen/diet/edit_diet.dart
@@ -0,0 +1,183 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:autocomplete_textfield/autocomplete_textfield.dart';
+import 'package:date_format/date_format.dart';
+import 'package:flutter/material.dart';
+import '../../name2phone.dart';
+import 'package:intl/intl.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AddDiet extends StatefulWidget {
+  AddDiet({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _AddDietState createState() => _AddDietState();
+}
+
+class _AddDietState extends State<AddDiet> {
+  double _height;
+  double _width;
+  // String _setTime, _setDate;
+
+  String _hour, _minute, _time;
+  String dateTime;
+  DateTime selectedDate = DateTime.now();
+  TimeOfDay selectedTime = TimeOfDay(hour: 00, minute: 00);
+  String phone;
+  String time;
+  String recipe;
+  List<String> therapy = [];
+  String therapytext = '';
+  String currentText = "";
+  var selected;
+  String selectedTherapy = '';
+  Timestamp date;
+
+  TextEditingController _timeController = TextEditingController();
+  TextEditingController _recipe = TextEditingController();
+
+  SimpleAutoCompleteTextField _therapyController;
+  GlobalKey<AutoCompleteTextFieldState<String>> key = new GlobalKey();
+  GlobalKey<AutoCompleteTextFieldState<String>> therapykey = new GlobalKey();
+
+  _AddDietState() {
+    _therapyController = SimpleAutoCompleteTextField(
+        key: therapykey,
+        decoration: new InputDecoration(
+            icon: const Icon(Icons.assignment), labelText: "Recipe Name"),
+        controller: _recipe,
+        suggestions: therapy,
+        textChanged: (text) => therapytext = text,
+        clearOnSubmit: false,
+        textSubmitted: (text) => setState(() {
+              if (text != "") {
+                selectedTherapy = text;
+              }
+            }));
+  }
+
+  Future<Null> _selectTime(BuildContext context) async {
+    final TimeOfDay picked = await showTimePicker(
+      context: context,
+      initialTime: selectedTime,
+    );
+    if (picked != null)
+      setState(() {
+        selectedTime = picked;
+        _hour = selectedTime.hour.toString();
+        _minute = selectedTime.minute.toString();
+        _time = _hour + ' : ' + _minute;
+        _timeController.text = _time;
+        _timeController.text = formatDate(
+            DateTime(2019, 08, 1, selectedTime.hour, selectedTime.minute),
+            [hh, ':', nn, " ", am]).toString();
+      });
+  }
+
+  @override
+  void initState() {
+    var data = jsonDecode(widget.title);
+    phone = data['phone'];
+    time = data['time'];
+    recipe = data['recipe'];
+    _recipe.text = recipe;
+    _timeController.text = time;
+    auto();
+    // _dateController.text = DateFormat.yMMMd().format(DateTime.now());
+
+    // _timeController.text = formatDate(
+    //     DateTime(2019, 08, 1, DateTime.now().hour, DateTime.now().minute),
+    //     [hh, ':', nn, " ", am]).toString();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _height = MediaQuery.of(context).size.height;
+    _width = MediaQuery.of(context).size.width;
+
+    Future<void> editDiet() async {
+      if (selectedTherapy == '') {
+        selectedTherapy = recipe;
+      }
+      print(time);
+      FirebaseFirestore.instance
+          .collection('Users')
+          .doc(phone + '/diet/' + time)
+          .delete();
+
+      // FirebaseFirestore.instance
+      //     .collection('Users')
+      //     .doc(phone + '/diet')
+      //     .set({_timeController.text: selectedTherapy});
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Diet'),
+      ),
+      body: SingleChildScrollView(
+        physics: NeverScrollableScrollPhysics(),
+        child: Container(
+          width: _width,
+          height: _height,
+          margin:
+              const EdgeInsets.only(top: 20, left: 15, right: 40, bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: _height / 40,
+                  ),
+                  InkWell(
+                    onTap: () {
+                      _selectTime(context);
+                    },
+                    child: IgnorePointer(
+                      child: new TextFormField(
+                        decoration: const InputDecoration(
+                          icon: const Icon(Icons.watch_later),
+                          hintText: 'Select Time',
+                          labelText: 'Time',
+                        ),
+                        controller: _timeController,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: _height / 40,
+                  ),
+                  _therapyController,
+                  SizedBox(
+                    height: _height / 3,
+                  ),
+                  RaisedButton(
+                    onPressed: () => {editDiet(), Navigator.pop(context)},
+                    color: Color(0xff3fc380),
+                    textColor: Colors.white,
+                    child: Text('Edit'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void auto() {
+    FirebaseFirestore.instance
+        .collection('Recipe')
+        .get()
+        .then((QuerySnapshot querySnapshot) => {
+              querySnapshot.docs.forEach((doc) {
+                therapy.add(doc.data()["Name"]);
+              }),
+            });
+  }
+}
